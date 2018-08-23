5f7fbff03ad08831776d7fdbe1867208423bf722
ysdy44 <ysdy44@outlook.com>
2018-08-23 21:22:38 +0800

    RadiusShadowPanel

--- FanKit/Frames/Template/SplitPanelPage.xaml.cs
+++ FanKit/Frames/Template/SplitPanelPage.xaml.cs
@@ -18,7 +18,7 @@ namespace FanKit.Frames.Template
         {
             this.MarkdownText1.Text = await FanKit.Library.File.GetFile("ms-appx:///TXT/Template/SplitPanelXaml.txt");
             this.MarkdownText2.Text = await FanKit.Library.File.GetFile("ms-appx:///TXT/Template/SplitPanelCs.txt");
-            this.MarkdownText3.Text = await FanKit.Library.File.GetFile("ms-appx:///TXT/Template/SplitPanelUserCs.txt");
+            this.MarkdownText3.Text = await FanKit.Library.File.GetFile("ms-appx:///TXT/Template/SplitPanelUserXaml.txt");
             this.MarkdownText4.Text = await FanKit.Library.File.GetFile("ms-appx:///TXT/Template/SplitPanelUserCs.txt");
         }
 
