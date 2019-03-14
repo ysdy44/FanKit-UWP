ffab0df41a6dcecbdf782635f814d82bd96b13d5
ysdy44 <ysdy44@outlook.com>
2019-03-15 00:44:23 +0800

    new projects [FanKit.Pickers], [FanKit.Win2Ds].

--- FanKit.Win2Ds/Properties/AssemblyInfo.cs
+++ FanKit.Win2Ds/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Win2Ds")]
+[assembly: AssemblyTitle("FanKit.Win2Ds")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Win2Ds")]
+[assembly: AssemblyProduct("FanKit.Win2Ds")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
