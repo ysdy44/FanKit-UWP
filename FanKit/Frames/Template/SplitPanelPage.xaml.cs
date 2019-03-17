80b8335ca64c596570ed07fed972ae7777bdc915
ysdy44 <ysdy44@outlook.com>
2019-03-17 18:53:27 +0800

    SplitPanelPage

--- FanKit/Frames/Template/SplitPanelPage.xaml.cs
+++ FanKit/Frames/Template/SplitPanelPage.xaml.cs
@@ -15,13 +15,8 @@ namespace FanKit.Frames.Template
         {
             this.MarkdownText1.Text = await FanKit.Sample.File.GetFile("ms-appx:///TXT/Template/SplitPanelPage.xaml.txt");
             this.MarkdownText2.Text = await FanKit.Sample.File.GetFile("ms-appx:///TXT/Template/SplitPanelPage.xaml.cs.txt");
-            this.MarkdownText3.Text = await FanKit.Sample.File.GetFile("ms-appx:///TXT/Template/SplitButton.xaml.txt");
-            this.MarkdownText4.Text = await FanKit.Sample.File.GetFile("ms-appx:///TXT/Template/SplitButton.cs.txt");
         }
-
-
-        #region Splite：侧栏
-
+               
 
 
         private double openOpacity;
@@ -33,7 +28,7 @@ namespace FanKit.Frames.Template
                 this.DismissOverlayBackground.Visibility = value == 0.0 ? Visibility.Collapsed : Visibility.Visible;
                 this.DismissOverlayBackground.Opacity = value;
                 this.SpliteLeftShadow.Opacity = value;
-                this.BehaviorOffset.OffsetX = (value - 1) * this.ContentPanel.ActualWidth;
+                this.BehaviorOffset.OffsetX = (value - 1) * this.ContentPresenter.ActualWidth;
 
                 this.openOpacity = value;
             }
@@ -46,7 +41,7 @@ namespace FanKit.Frames.Template
             get
             {
                 if (translateX > 0.0d) return 0.0d;
-                else if (translateX < -this.ContentPanel.ActualWidth) return -this.ContentPanel.ActualWidth;
+                else if (translateX < -this.ContentPresenter.ActualWidth) return -this.ContentPresenter.ActualWidth;
                 else return translateX;
             }
         }
@@ -54,21 +49,20 @@ namespace FanKit.Frames.Template
         private new void ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
         {
             this.BehaviorOffset.Duration = 0.0d;
-            this.TranslateX = (this.OpenOpacity - 1) * this.ContentPanel.ActualWidth; 
+            this.TranslateX = (this.OpenOpacity - 1) * this.ContentPresenter.ActualWidth; 
         }
         private new void ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
         {
             this.translateX += e.Delta.Translation.X;
-            this.OpenOpacity = this.TranslateX / this.ContentPanel.ActualWidth + 1;
+            this.OpenOpacity = this.TranslateX / this.ContentPresenter.ActualWidth + 1;
         }
         private new void ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
         {
             this.BehaviorOffset.Duration = 600.0d;
-            this.OpenOpacity = this.TranslateX > -this.ContentPanel.ActualWidth / 2? 1.0: 0.0;
+            this.OpenOpacity = this.TranslateX > -this.ContentPresenter.ActualWidth / 2? 1.0: 0.0;
         }
 
         
-        #endregion
 
 
         private void Panel_Tapped(object sender, TappedRoutedEventArgs e) => this.OpenOpacity = 0.0d;
