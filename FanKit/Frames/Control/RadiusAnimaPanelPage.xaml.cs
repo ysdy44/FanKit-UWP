cf477746348386bf4e0aa14a14bf12053087eaa7
ysdy44 <ysdy44@outlook.com>
2019-05-03 18:17:56 +0800

    [Stage] RemoteControl

--- FanKit/Frames/Control/RadiusAnimaPanelPage.xaml.cs
+++ FanKit/Frames/Control/RadiusAnimaPanelPage.xaml.cs
@@ -1,22 +1,9 @@
 ﻿using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Input;
 
 namespace FanKit.Frames.Control
 {
     public sealed partial class RadiusAnimaPanelPage : Page
     {
-        public RadiusAnimaPanelPage()
-        {
-            this.InitializeComponent();
-            this.Loaded += async (sender, e) =>
-            {
-                this.MarkdownText1.Text = await FanKit.Samples.File.GetFile("ms-appx:///TXT/Control/RadiusAnimaPanelPage.xaml.txt");
-                this.MarkdownText2.Text = await FanKit.Samples.File.GetFile("ms-appx:///TXT/Control/RadiusAnimaPanel.xaml.txt");
-                this.MarkdownText3.Text = await FanKit.Samples.File.GetFile("ms-appx:///TXT/Control/RadiusAnimaPanel.cs.txt");
-            };
-        }
-         
-
         double WidthText
         {
             set
@@ -26,12 +13,23 @@ namespace FanKit.Frames.Control
             }
         }
 
-        private void Button50_Tapped(object sender, TappedRoutedEventArgs e) => this.WidthText = 50;
-        private void Button100_Tapped(object sender, TappedRoutedEventArgs e) => this.WidthText = 100;
-        private void Button150_Tapped(object sender, TappedRoutedEventArgs e) => this.WidthText = 150;
-        private void Button200_Tapped(object sender, TappedRoutedEventArgs e) => this.WidthText = 200;
-        private void Button250_Tapped(object sender, TappedRoutedEventArgs e) => this.WidthText = 250;
-        private void Button300_Tapped(object sender, TappedRoutedEventArgs e) => this.WidthText = 300;
+        public RadiusAnimaPanelPage()
+        {
+            this.InitializeComponent();
 
+            this.Button50.Tapped += (s, e) => this.WidthText = 50;
+            this.Button100.Tapped += (s, e) => this.WidthText = 100;
+            this.Button150.Tapped += (s, e) => this.WidthText = 150;
+            this.Button200.Tapped += (s, e) => this.WidthText = 200;
+            this.Button250.Tapped += (s, e) => this.WidthText = 250;
+            this.Button300.Tapped += (s, e) => this.WidthText = 300;
+
+            this.Loaded += async (sender, e) =>
+            {
+                this.MarkdownText1.Text = await FanKit.Samples.File.GetFile("ms-appx:///TXT/Control/RadiusAnimaPanelPage.xaml.txt");
+                this.MarkdownText2.Text = await FanKit.Samples.File.GetFile("ms-appx:///TXT/Control/RadiusAnimaPanel.xaml.txt");
+                this.MarkdownText3.Text = await FanKit.Samples.File.GetFile("ms-appx:///TXT/Control/RadiusAnimaPanel.cs.txt");
+            };
+        }      
     }
 }
---
+++ FanKit/Frames/Control/RemoteControlPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+using Windows.UI.Xaml.Controls;
+
+namespace FanKit.Frames.Control
+{
+    public sealed partial class RemoteControlPage : Page
+    {
+        Vector2 v;
+        private Vector2 vector;
+        public Vector2 Vector
+        {
+            get => this.vector;
+            set
+            {
+                this.RunX.Text = ((int)value.X).ToString();
+                this.RunY.Text = ((int)value.Y).ToString();
+
+                Canvas.SetLeft(this.Ellipse,value.X);
+                Canvas.SetTop(this.Ellipse, value.Y);
+
+                this.vector = value;
+            }
+        }
+
+        public RemoteControlPage()
+        {
+            this.InitializeComponent();
+
+            this.Vector = Vector2.Zero;
+
+            this.RemoteControl.Moved += (s, value) => this.Vector += value;
+            this.RemoteControl.ValueChangeStarted += (s, value) => this.v = this.Vector;
+            this.RemoteControl.ValueChangeDelta += (s, value) => this.Vector = this.v + value;
+            this.RemoteControl.ValueChangeCompleted += (s, value) => { };
+
+            this.Loaded += async (sender, e) =>
+            {
+                this.MarkdownText1.Text = await FanKit.Samples.File.GetFile("ms-appx:///TXT/Control/RemoteControlPage.xaml.txt");
+                this.MarkdownText2.Text = await FanKit.Samples.File.GetFile("ms-appx:///TXT/Control/RemoteControl.xaml.txt");
+                this.MarkdownText3.Text = await FanKit.Samples.File.GetFile("ms-appx:///TXT/Control/RemoteControl.cs.txt");
+            };
+        }
+    }
+}
